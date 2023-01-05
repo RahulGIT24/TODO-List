c36b72c49d0b90536d5bb78a34e76044cd5f4297
Rahul <rg4005450@gmail.com>
2023-01-05 21:50:37 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,32 +1,66 @@
-add.addEventListener("click", () => {
-  if (title.value == "" || desc.value == "") {
-    alert("Title or description can't be empty");
+function update() {
+  if (localStorage.getItem("items") == null) {
+    itemsJSONArray = [];
+    localStorage.setItem("items", JSON.stringify(itemsJSONArray));
   } else {
-    if (localStorage.getItem("itemsJSON") == null) {
-      itemsJSONArray = [];
-      itemsJSONArray.push([title.value, desc.value]);
-      localStorage.setItem("itemsJSON", JSON.stringify(itemsJSONArray));
-      localStorage.getItem("itemsJSON");
-    } else {
-      itemsJSONArray = localStorage.getItem("itemsJSON");
-      itemsJSONArray = JSON.parse(itemsJSONArray);
-      itemsJSONArray.push([title.value, desc.value]);
-      localStorage.setItem("itemsJSON", JSON.stringify(itemsJSONArray));
-    }
-
-    let str = "";
-    itemsJSONArray.forEach((element, index) => {
-      tableBody.innerHTML = `
-    <tr>
+    itemsJSONArrayStr = localStorage.getItem("items");
+    itemsJSONArray = JSON.parse(itemsJSONArrayStr);
+  }
+  // Populate the table
+  let str = "";
+  itemsJSONArray.forEach((element, index) => {
+    str += `
+  <tr>
               <th scope="row">${index + 1}</th>
               <td>${element[0]}</td>
               <td>${element[1]}</td>
               <td>
-                <button type="button" class="btn btn-sm btn-outline-danger">
+                <button type="button" class="btn btn-sm btn-outline-danger" onclick=deleted(${index})>
                   Delete
                 </button>
               </td>
-            </tr>`;
-    });
+            </tr>
+  `;
+  });
+  tableBody.innerHTML = str;
+}
+
+function getAndUpdate() {
+  console.log("Updating");
+  if (title.value == "" || desc.value == "") {
+    alert("Title or Description can't be empty");
+  } else {
+    if (localStorage.getItem("items") == null) {
+      itemsJSONArray = [];
+      itemsJSONArray.push([title.value, desc.value]);
+      localStorage.setItem("items", JSON.stringify(itemsJSONArray));
+    } else {
+      itemsJSONArrayStr = localStorage.getItem("items");
+      itemsJSONArray = JSON.parse(itemsJSONArrayStr);
+      itemsJSONArray.push([title.value, desc.value]);
+      localStorage.setItem("items", JSON.stringify(itemsJSONArray));
+    }
   }
+  update();
+}
+
+function TODOclear() {
+  console.log("Clearing...");
+  localStorage.clear();
+  update();
+}
+
+function deleted(itemIndex) {
+  console.log("Deleting");
+  itemsJSONArrayStr = localStorage.getItem("items");
+  itemsJSONArray = JSON.parse(itemsJSONArrayStr);
+  itemsJSONArray.splice(itemIndex, 1);
+  localStorage.setItem("items", JSON.stringify(itemsJSONArray));
+  update();
+}
+
+add.addEventListener("click", () => {
+  getAndUpdate();
 });
+
+update();
